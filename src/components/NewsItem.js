0362fed835cb75fa03a4b966d0df71464ad78eb7
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-11-26 16:31:38 +0530

    news api system created

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-    // kabootar = 56;
 
     render() {
-        let { title, description } = this.props
+        let { title, description, urlToImage } = this.props
         return (
             <div>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src="https://media.wired.com/photos/672bda0a90a94384370310f4/191:100/w_1280,c_limit/business_crypto_faithful_trump.jpg" alt="Error Loading ..." />
+                    <img src={!urlToImage ? "https://cff2.earth.com/uploads/2024/11/24171647/earth_tilted_31-inches_groundwater-use-humans_1m.jpg" : urlToImage} alt="Error Loading ..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="/towork" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
 
--- src/components/NewsMain.js
+++ src/components/NewsMain.js
@@ -2,55 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class NewsMain extends Component {
-    totalNews = {
-        "status": "ok",
-        "totalResults": 2,
-        "articles": [
-            {
-                "source": {
-                    "id": null,
-                    "name": "Sports Illustrated"
-                },
-                "author": "Patrick Andres",
-                "title": "Kylie Kelce, Wife of Jason Kelce, Announces Pregnancy With Couple's Fourth Child - Sports Illustrated",
-                "description": "A big week for the family continues.",
-                "url": "https://www.si.com/nfl/kylie-kelce-jason-kelce-announces-pregnancy-fourth-child",
-                "urlToImage": "https://images2.minutemediacdn.com/image/upload/c_crop,w_2135,h_1200,x_763,y_0/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/images/ImagnImages/mmsport/si/01jddfrzyh70sqyttk6e.jpg",
-                "publishedAt": "2024-11-23T22:15:01Z",
-                "content": "Former Philadelphia Eagles center Jason Kelce's family is about to get bigger.\r\nKelce's wife, Kylie, is pregnant with her fourth child. According to the AP, agent Emily Ries confirmed the pregnancy S… [+819 chars]"
-            },
-            {
-                "source": {
-                    "id": null,
-                    "name": "UPI.com"
-                },
-                "author": null,
-                "title": "CDC confirms first child in United States infected with bird flu - UPI News",
-                "description": "UPI delivers the latest headlines from around the world: Top News, Entertainment, Health, Business, Science and Sports News - United Press International",
-                "url": "https://www.upi.com",
-                "urlToImage": "//www.upi.com/img/upi-fb.png",
-                "publishedAt": "2024-11-23T15:39:55Z",
-                "content": ""
-            }
-        ]
+    totalNews = []
+    constructor() {
+        super();
+        this.state = {
+            totalNews: this.totalNews,
+        }
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8");
+
+        const parsedJsonOfData = await data.json();
+        this.setState({
+            totalNews: parsedJsonOfData.articles
+        })
+
     }
     render() {
-        console.log(this.totalNews.articles)
+        console.log("This is my render")
         return (
             <div className='container'>
                 <h3 className="text-center my-2">
                     Durgesh News Website - Daily Updates
                 </h3>
                 <div className="row my-4">
-                    <div className="col-md-4">
-                        <NewsItem title={this.totalNews.articles[0].title} description="This is my description is not good" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Durgesh is good boy2" description="This is my description is not good" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Durgesh is good boy3" description="This is my description is not good" />
-                    </div>
+                    {this.state.totalNews.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title} description={element.description} urlToImage={element.urlToImage} />
+                        </div>
+                    })}
                 </div>
             </div>
         )
