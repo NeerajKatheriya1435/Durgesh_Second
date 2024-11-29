29c160de4395f2eddf71f3c83a8b91c5bc93c5c4
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-11-29 16:08:09 +0530

    some minor issue fixed like nex and prevoius

--- src/components/NewsMain.js
+++ src/components/NewsMain.js
@@ -12,33 +12,37 @@ export class NewsMain extends Component {
     }
 
     async componentDidMount() {
-        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=1`);
-
+        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=1&pageSize=20`);
         const parsedJsonOfData = await data.json();
         this.setState({
             totalNews: parsedJsonOfData.articles,
+            totalResults: parsedJsonOfData.totalResults
         })
 
     }
 
-
+    // 36/20---->2
     render() {
         const handleNext = async () => {
-            const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=${this.state.page + 1}`);
 
-            const parsedJsonOfData = await data.json();
-            this.setState({
-                totalNews: parsedJsonOfData.articles,
-                // page: this.state.page + 1
-            })
+            if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+            } else {
+                const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=${this.state.page + 1}&pageSize=20`);
+                const parsedJsonOfData = await data.json();
+                this.setState({
+                    page: this.state.page + 1,
+                    totalNews: parsedJsonOfData.articles
+                })
+            }
+
         }
         const handlePrevious = async () => {
-            const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=${this.state.page - 1}`);
-
+            const data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f6e9b5eb495049f2a2c81bd02fede1d8&page=${this.state.page - 1}&pageSize=20`);
             const parsedJsonOfData = await data.json();
             this.setState({
-                totalNews: parsedJsonOfData.articles,
-                // page: this.state.page - 1
+                page: this.state.page - 1,
+                totalNews: parsedJsonOfData.articles
             })
         }
         return (
@@ -48,13 +52,13 @@ export class NewsMain extends Component {
                 </h3>
                 <div className="row my-4">
                     {this.state.totalNews.map((element) => {
-                        return <div className="col-md-4" key={!element.url ? element.urlToImage : element.url}>
+                        return <div className="col-md-4" key={element.urlToImage ? element.urlToImage : element.url}>
                             <NewsItem title={element.title} description={element.description} urlToImage={element.urlToImage} />
                         </div>
                     })}
                 </div>
                 <div className="container d-flex justify-content-between">
-                    <button className="btn btn-primary my-2" onClick={handlePrevious}>Previous</button>
+                    <button disabled={this.state.page <= 1} className="btn btn-primary my-2" onClick={handlePrevious}>Previous</button>
                     <button className="btn btn-primary my-2" onClick={handleNext}>Next</button>
                 </div>
             </div>
